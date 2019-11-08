a2ad7f4c681100fbf1ff0a6f80b5f0a074809062
austinrafter <56013277+austinrafter@users.noreply.github.com>
2019-11-08 12:40:50 -0800

    Deleted function, changed variables
    1)Deleted the function because i found it was unnecessary 
    2)changed the post to choose through the radio buttons
    3) set variables for rushed, standard, and same_day
    4) set conditional ifs for value or radio button to be equal to the string, if so it should set the session variables
    5)forces through to next page with same day choice without user choosing

--- shipping.php
+++ shipping.php
@@ -13,6 +13,9 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true)
 
 ?>
 
+
+
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -26,25 +29,45 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true)
     session_start();
     $downtown_zip = '95113'; ?>
 
-    <script type="text/javascript">
-            // the selector will match all input controls of type :checkbox
-        // and attach a click event handler
-        $("input:checkbox").on('click', function() {
-        // in the handler, 'this' refers to the box clicked on
-        var $box = $(this);
-        if ($box.is(":checked")) {
-          // the name of the box is retrieved using the .attr() method
-          // as it is assumed and expected to be immutable
-          var group = "input:checkbox[name='" + $box.attr("name") + "']";
-          // the checked state of the group/box on the other hand will change
-          // and the current value is retrieved using .prop() method
-          $(group).prop("checked", false);
-          $box.prop("checked", true);
-        } else {
-          $box.prop("checked", false);
+
+    <?php
+
+        if (isset($_POST['continuetofinish']))
+        {
+          $selected_option = $_POST["group"];
+          $rushed = "rushed";
+          $standard = "standard";
+
+          if( $downtown_zip === $_SESSION["zip"])
+          {
+            $same_day = "same_day";
+
+            if ($selected_option === $same_day)
+            {
+              $_SESSION["shipping_time"] = "Your groceries will be there by the end of the day";
+              $_SESSION["shippingsameday"] = 5;
+              header("location: fooddelivery.php");
+            }
+          }
+
+            if ($selected_option === $rushed)
+            {
+              $_SESSION["shipping_time"] = "Your groceries will ship in 1 day";
+              $_SESSION["shippingrushed"] = 2;
+              header("location: fooddelivery.php");
+            }
+
+            if($selected_option ===$standard)
+            {
+              $_SESSION["shipping_time"] = "Your groceries will ship in 2-3 days";
+              $_SESSION["shippingstandard"]  = 0;
+              header("location: fooddelivery.php");
+            }
+
+
         }
-        });
-    </script>
+
+          ?>
     <div>
     <h3>Shipping Options</h3>
 
@@ -52,14 +75,14 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true)
     if( $downtown_zip === $_SESSION["zip"]){ ?>
 
       <label>
-        <input type="radio" class="radio" value="same_day" id="same_day" name="group[1][]" />Same Day Shipping</label>
+        <input type="radio" class="radio" value="same_day" id="same_day" name="group" />Same Day Shipping</label>
       <?php
       }
   ?>
 <label>
-  <input type="radio" class="radio" value="rushed" id="rushed" name="group[1][]" />Rushed 1 Day Shipping</label>
+  <input type="radio" class="radio" value="rushed" id="rushed" name="group" />Rushed 1 Day Shipping</label>
 <label>
-  <input type="radio" class="radio" value="standard" id="standard" name="group[1][]" />Standard 2-3 Day Shipping</label>
+  <input type="radio" class="radio" value="standard" id="standard" name="group" />Standard 2-3 Day Shipping</label>
 </label>
 <br>
   <input type="submit" value="Continue to finish order" class="btn" name="continuetofinish">
@@ -71,50 +94,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true)
 
 
 
-<?php
-
-    if (isset($_POST['continuetofinish']))
-    {
-      ?>
-      <script type="text/javascript">
-        var sameDay = document.getElementById("same_day").value;
-          var rushed = document.getElementById("rushed").value;
-          var standard = document.getElementById("standard").value;
-
-          <?php $_SESSION["same_day"] = "<script>document.write(sameDay)</script>";
-                $_SESSION["rushed"] = "<script>document.write(rushed)</script>";
-                $_SESSION["standard"] = "<script>document.write(standard)</script>";
-           ?>
-
-          </script>
-        <?php
-
-
-        if (isset($_SESSION["same_day"]))
-        {
-          $_SESSION["shipping_time"] = "Your groceries will be there by the end of the day";
-          $_SESSION["shippingsameday"] = 5;
-          header("location: fooddelivery.php");
-        }
-        elseif (isset($_SESSION["rushed"]))
-        {
-          $_SESSION["shipping_time"] = "Your groceries will ship in 1 day";
-          $_SESSION["shippingrushed"] = 2;
-          header("location: fooddelivery.php");
-        }
-        elseif(isset($_SESSION["standard"]))
-        {
-          $_SESSION["shipping_time"] = "Your groceries will ship in 2-3 days";
-          $_SESSION["shippingstandard"]  = 0;
-          header("location: fooddelivery.php");
-        }
-        else{
-          echo "you did not make a choice";
-        }
-
-    }
-
-      ?>
 
   </body>
 </html>
+
